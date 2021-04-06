dfa73a3b7c08945ecdd10fa9af75fd2932fc4257
Samantha-Grace <sgrace732@gmail.com>
2021-04-06 13:50:04 -0600

    updating struct

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
-//creating a new struct
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	var alex person
+
+	//updating structs
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+	fmt.Printf("%+v", alex)
+}
+
+/*
 type person struct {
 	firstName string
 	lastName  string
@@ -8,4 +24,19 @@ type person struct {
 
 func main() {
 	alex := person{"Alex", "Anderson"}
+	fmt.Println(alex)
 }
+
+
+//second way
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	alex := person{firstName: "Alex", lastName: "Anderson"}
+	fmt.Println(alex)
+}
+*/
