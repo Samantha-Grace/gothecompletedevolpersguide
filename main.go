d3678aface6a41ce3204e70069e146b65fedad8b
Samantha-Grace <sgrace732@gmail.com>
2021-04-07 09:55:26 -0600

    embedded struct inside another struct

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type cat struct{}
-
-func (c *cat) say() {
-	fmt.Println("meow")
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	cat := &cat{}
-	cat.say()
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Smith",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 560899,
+		},
+	}
+	fmt.Printf("%+v", jim)
 }
+
+//embedded struct inside another
