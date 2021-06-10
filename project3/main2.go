602d28b66c3a2e36cb872a94414932beaae3bf13
Samantha-Grace <sgrace732@gmail.com>
2021-06-10 14:03:49 -0600

    insert pause

--- project3/main2.go
+++ project3/main2.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -20,8 +21,11 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for {
-		go checkLink(<-c, c)
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second) //pause for 5 seconds
+			checkLink(link, c)
+		}(l)
 	}
 }
 
